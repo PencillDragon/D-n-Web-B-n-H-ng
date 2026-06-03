07e8f7a9346cfab7403879c774c9b010e8fab081
PencillDragon <rewasvodich@gmail.com>
2026-06-03 21:34:49 +0700

    gộp vào

--- app.py
+++ app.py
@@ -15,6 +15,84 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 db = SQLAlchemy(app)
 
+<<<<<<< Updated upstream
+=======
+# người dùng
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+
+
+with app.app_context():
+    db.create_all()
+
+# đăng ký tài khoản
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        user = User(
+            username=request.form['username'],
+            password=request.form['password'],
+            address=request.form['address']
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template("register.html")
+
+# đăng nhập người dùng
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        user = User.query.filter_by(
+            username=request.form['username'],
+            password=request.form['password']
+        ).first()
+
+        if user:
+            session.clear()
+
+            session["role"] = "user"
+            session["user"] = user.username
+            return redirect("/")
+
+        return "Sai login"
+
+    return render_template("login.html")
+
+# đăng nhập admin
+ADMIN_USER = "admin"
+ADMIN_PASS = "123456"
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+
+    if request.method == 'POST':
+
+        if request.form['username'] == ADMIN_USER and request.form['password'] == ADMIN_PASS:
+            session.clear()
+            session["role"] = "admin"
+            return redirect("/")
+
+        return "Sai admin"
+
+    return render_template("admin.html")
+
+# đăng xuất
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/login")
+
+>>>>>>> Stashed changes
 # giỏ hàng
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
